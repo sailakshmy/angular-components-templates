407bcaaccc0155123ea85b13fa6c22a11c2ae83c
Sai <sailakshmy94@gmail.com>
2025-07-15 17:29:27 +0400

    State management and cross component communication

--- src/app/components/dashboard/tickets/tickets.component.ts
+++ src/app/components/dashboard/tickets/tickets.component.ts
@@ -22,4 +22,16 @@ export class TicketsComponent {
     };
     this.tickets.push(ticket);
   }
+
+  onCloseTicket(id: string) {
+    this.tickets = this.tickets.map((ticket) => {
+      if (ticket.id === id) {
+        return {
+          ...ticket,
+          status: "closed",
+        };
+      }
+      return ticket;
+    });
+  }
 }
